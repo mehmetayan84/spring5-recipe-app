0c3f9454639e2b1ed833d12e1235f8ac2c75d3ba
MehmetAyan <mehmetayan84@gmail.com>
2018-09-28 01:07:32 +0300

    Ingredients and unit of measure is added to project as entity.

--- src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5recipeapp.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -17,6 +18,9 @@ public class Recipe {
     private String directions;
     //todo add
     //private Difficulty difficulty;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> igredients;
 
     @Lob
     private Byte[] image;
@@ -102,5 +106,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIgredients() {
+        return igredients;
+    }
+
+    public void setIgredients(Set<Ingredient> igredients) {
+        this.igredients = igredients;
     }
 }
