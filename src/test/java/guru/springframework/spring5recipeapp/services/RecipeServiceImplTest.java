72a1500a7788c1e2df82a71ac5639f945edf4a73
MehmetAyan <mehmetayan84@gmail.com>
2018-10-15 19:37:48 +0300

    All the unit tests and integration test are implemented.

---
+++ src/test/java/guru/springframework/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,50 @@
+package guru.springframework.spring5recipeapp.services;
+
+import guru.springframework.spring5recipeapp.domain.Recipe;
+import guru.springframework.spring5recipeapp.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+
+    }
+
+    @Test
+    public void getRecipes() {
+
+        Recipe recipe = new Recipe();
+
+        Set<Recipe> recipeData = new HashSet<>();
+
+        recipeData.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipeData);
+
+        Set<Recipe> recipeSet = recipeService.getRecipes();
+
+        assertEquals(recipeSet.size(), 1);
+
+        verify(recipeRepository, times(1)).findAll();
+
+    }
+}
